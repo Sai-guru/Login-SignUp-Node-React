bc08fb48bbdd5c6801151aea43f5c4544e319efe
Sai-guru <lpnkaveri@gmail.com>
2025-03-29 23:55:09 +0530

    Your descriptive commit message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Change 'dist' to 'build'
+    outDir: 'dist', // Change 'dist' to 'build'
   },
 });
